b223e3919c310eccbe629185de6e33b89be8e7e9
AtroMusic <sadeqatro916@gmail.com>
2025-01-31 16:57:42 +0530

    Update config.py
    Signed-off-by: AtroMusic <sadeqatro916@gmail.com>

--- config/config.py
+++ config/config.py
@@ -50,7 +50,7 @@ EXTRA_PLUGINS = getenv("EXTRA_PLUGINS", True)
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/TheTeamVivek/Extra-Plugin",
+    "https://github.com/AtroMusic/Atro_music_plugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
